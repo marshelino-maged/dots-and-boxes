dc26031ccc91c9470ab6cd7dc300266238c8577a
marshelino-maged <78962756+marshelino-maged@users.noreply.github.com>
2021-12-23 17:04:04 +0200

    update menu.h
    dah al menu al gdid b3d ma zabato

--- menu.h
+++ menu.h
@@ -1,31 +1,120 @@
-int print_menu(){
-    int i=0;
+void print_menu(){
+    int i=0,j;
     printf(blue"\n\n\n\t\t\t\t\tWelcome to our game\n\n\t\t\t\t\t  Dots and Boxes\n\n\n\n\n");
-    printf(yellow"\t 1-new game\n \t 2-load game\n \t 3-top ten\n \t 4-exit\n\n \t choose from 1 to 4 : "reset);
-    while(i==0){
-        scanf("%d",&i);
+    printf(yellow"\t 1 - new game\n \t 2 - load game\n \t 3 - top ten\n \t 4 - exit\n\n \t choose from 1 to 4 : "reset);
+    while (1){
+        i = scanint();
         if((i==1) || (i==2) || (i==3) || (i==4)){
-            return i;
+            break;
         }
         else{
             printf(red"\t ERROR, please enter a number from 1 to 4 : "reset);
-            i = 0;
+        }
+
+    }
+    system("cls");
+    if(i==1){
+        printf(yellow "\n\n\n\n\n\n\n\n\t 1 - Easy \n \t 2 - Medium \n \t 3 - Hard \n \t 4 - return to the main menu \n\t enter number from 1 to 4 : " reset);
+        while(1){
+            i = scanint();
+            if(i==1){
+                system("cls");
+                printf(yellow"\n\n\n\t 1 - 1 player \n\t 2 - 2 players \n \t please enter a number: "reset);
+                while(1){
+                    i = scanint();
+                    if(i==1){
+                        //function to play easy against computer
+                        return;
+                    }
+                    else if(i==2){
+                        //function to play easy against your friend
+                        return;
+                    }
+                    else{
+                        printf(red"\t ERROR, please enter a number from 1 to 2 : "reset);
+                    }
+                }
+            }
+            else if(i==2){
+                system("cls");
+                printf(yellow"\n\n\n\t 1 - 1 player \n\t 2 - 2 players \n \t please enter a number: "reset);
+                while(1){
+                    i = scanint();
+                    if(i==1){
+                        //function to play medium against computer
+                        return;
+                    }
+                    else if(i==2){
+                        //function to play medium against your friend
+                        return;
+                    }
+                    else{
+                        printf(red"\t ERROR, please enter a number from 1 to 2 : "reset);
+                    }
+                }
+            }
+            else if(i==3){
+                printf(yellow"\t 1 - 1 player \n\t 2 - 2 players \n \t please enter a number: "reset);
+                while(1){
+                    i = scanint();
+                    if(i==1){
+                        //function to play hard against computer
+                        return;
+                    }
+                    else if(i==2){
+                        //function to play hard against your friend
+                        return;
+                    }
+                    else{
+                        printf(red"\t ERROR, please enter a number from 1 to 2 : "reset);
+                    }
+                }
+            }
+            else if(i==4){
+                system("cls");
+                print_menu();
+                }
+            else{
+                printf(red"\t ERROR, please enter a number from 1 to 4 : "reset);
+            }
         }
     }
-}
-void print_menu2(int i){
-    int j;
-    if(i==1){
-        printf(blue "\t 1-Easy \n \t 2-Medium \n \t 3-Hard \n\t enter number from 1 to 3 : " reset);
-    }
     else if(i==2){
-        printf(blue "\t 1-Load game 1 \n \t 2-Load game 2 \n \t 3-Load game 3 \n\t Enter number from 1 to 3 : " reset);
+        printf(yellow "\n\n\n\n\n\n\n\n\t 1 - Load game 1 \n \t 2 - Load game 2 \n \t 3 - Load game 3 \n \t 4 - return to the main menu \n\t Enter number from 1 to 4 : " reset);
+        while(1){
+            i = scanint();
+            if(i==1){
+                //load game one
+                return;
+            }
+            else if(i==2){
+                //load game two
+                return;
+            }
+            else if(i==3){
+                //load game three
+                return;
+            }
+            else if(i==4){
+                system("cls");
+                print_menu();
+            }
+            else{
+                printf(red"\t ERROR, please enter a number from 1 to 4 : "reset);
+            }
+        }
     }
     else if(i==3){
-        printf(yellow "\n \t the top 10 are" reset);
+        printf(yellow "\n\n\n\n\n\n\n\n\n \t the top 10 are\n");
+        printf("\t press enter to return to the main menu:"reset);
+        scanint();
+        system("cls");
+        print_menu();
+
     }
     else if(i==4){
         exit(0);
     }
     return;
 }
+
